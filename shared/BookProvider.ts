05bee3d60ced895e4b7538f687eb8fb784c92079
Stephen McMahon <smcmahon@slb.com>
2020-12-17 16:05:07 -0600

    more cleanup

--- shared/BookProvider.ts
+++ shared/BookProvider.ts
@@ -4,44 +4,47 @@ import { Book } from './Book';
 import { DataProvider } from './DataProvider';
 import { QueryArgs } from './QueryArgs';
 
-export class BookProvider extends BaseProvider implements DataProvider<Book> {
-    private sqlNewBook: string = "insert into Books (isbn, title, authors, tags) values (@isbn, @title, @authors, @tags);";
-    private sqlGetBook: string = "select isbn, title, authors, tags from books where isbn = @isbn;";
-    private sqlGetAllBooks: string = "select isbn, title, authors, tags from books;";
-    private sqlUpdateBook: string = "update books set title = @title, authors = @authors, tags = @tags where isbn = @isbn";
-    private sqlDeleteBook: string = "delete from books where isbn = @isbn";
+const ISBN: string = "isbn";
+const Title: string = "title";
+const Authors: string = "authors";
+const Tags: string = "tags";
 
+const sqlNewBook: string = "insert into Books (isbn, title, authors, tags) values (@isbn, @title, @authors, @tags);";
+const sqlGetBook: string = "select isbn, title, authors, tags from books where isbn = @isbn;";
+const sqlGetAllBooks: string = "select isbn, title, authors, tags from books;";
+const sqlUpdateBook: string = "update books set title = @title, authors = @authors, tags = @tags where isbn = @isbn";
+const sqlDeleteBook: string = "delete from books where isbn = @isbn";
+
+export class BookProvider extends BaseProvider implements DataProvider<Book> {
     constructor(dbConfig: any) {
         super(dbConfig);
     }
 
-    async create(value: Book): Promise<void> {
-        let args: QueryArgs[] = [
-            { name: "isbn", type: VarChar, value: value.isbn },
-            { name: "title", type: NVarChar, value: value.title },
-            { name: "authors", type: NVarChar, value: value.authors },
-            { name: "tags", type: NVarChar, value: value.tags }
+    private getArgs(value: Book): QueryArgs[] {
+        return [
+            { name: ISBN, type: VarChar, value: value.isbn },
+            { name: Title, type: NVarChar, value: value.title },
+            { name: Authors, type: NVarChar, value: value.authors },
+            { name: Tags, type: NVarChar, value: value.tags }
         ];
+    }
 
-        await this.executeQuery(this.sqlNewBook, args);
+    private getIsbnArg(isbn: string): QueryArgs[] {
+        return [
+            { name: ISBN, type: VarChar, value: isbn }
+        ];
+    }
+
+    async create(value: Book): Promise<void> {
+        await super.executeQuery(sqlNewBook, this.getArgs(value));
     }
 
     async update(value: Book): Promise<void> {
-        let args: QueryArgs[] = [
-            { name: "isbn", type: VarChar, value: value.isbn },
-            { name: "title", type: VarChar, value: value.title },
-            { name: "authors", type: NVarChar, value: value.authors },
-            { name: "tags", type: NVarChar, value: value.tags }
-        ];
-
-        await this.executeQuery(this.sqlUpdateBook, args);
+        await super.executeQuery(sqlUpdateBook, this.getArgs(value));
     }
 
-    async get(id: string): Promise<Book> {
-        let args: QueryArgs[] = [
-            { name: "isbn", type: VarChar, value: id }
-        ];
-        let records = await this.executeQuery(this.sqlGetBook, args);
+    async get(isbn: string): Promise<Book> {
+        let records = await super.executeQuery(sqlGetBook, this.getIsbnArg(isbn));
         if (records.length > 0) {
             let rec = records[0];
             return { isbn: rec.isbn, title: rec.title, authors: rec.authors, tags: rec.tags };
@@ -51,15 +54,12 @@ export class BookProvider extends BaseProvider implements DataProvider<Book> {
 
     async getAll(): Promise<Book[]> {
         let books: Book[] = [];
-        let records = await this.executeQuery(this.sqlGetAllBooks, []);
+        let records = await super.executeQuery(sqlGetAllBooks, []);
         records.forEach(r => books.push({ isbn: r.isbn, title: r.title, authors: r.authors, tags: r.tags }));
         return books;
     }
 
-    async delete(id: string): Promise<void> {
-        let args: QueryArgs[] = [
-            { name: "isbn", type: VarChar, value: id }
-        ];
-        await this.executeQuery(this.sqlDeleteBook, args);
+    async delete(isbn: string): Promise<void> {
+        await super.executeQuery(sqlDeleteBook, this.getIsbnArg(isbn));
     }
 }
